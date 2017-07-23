86bccfc4843b41a39a19bfc3965128f296a42376
Maxim Genko <gen-maksim@mail.ru>
2017-07-23 16:07:02 +0200

    robot changes

--- src/main/java/Robots/Robot_Agent.java
+++ src/main/java/Robots/Robot_Agent.java
@@ -8,9 +8,14 @@ import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.lang.acl.ACLMessage;
 import jade.domain.FIPAException;
+import jade.domain.FIPANames;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.FailureException;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetResponder;
 
 public class Robot_Agent extends CommonAgent {
 	
@@ -18,9 +23,8 @@ public class Robot_Agent extends CommonAgent {
 	protected void setup() {
 		/* forming service description */
 		ServiceDescription serviceDescription = new ServiceDescription();
-		serviceDescription.setName("printing");
-		serviceDescription.setType("office-service");
-		/* forming agent description. one agent can provide several services */
+		serviceDescription.setName("boiling");
+		serviceDescription.setType("Coffee-machine");
 		DFAgentDescription agentDescription = new DFAgentDescription();
 		agentDescription.setName(getAID());
 		agentDescription.addServices(serviceDescription);
@@ -32,35 +36,32 @@ public class Robot_Agent extends CommonAgent {
 		}
 
 		/* registering Behaviours to react for different types of messages */
-		addBehaviour(new HandleAcceptProposal());
-		addBehaviour(new HandleCallForProposal());
+		addBehaviour(new HandleCallForProposal(this));
 	}
 	
-	class HandleCallForProposal extends CyclicBehaviour {
+	class HandleCallForProposal extends ContractNetResponder {
 		private static final long serialVersionUID = 2429876704345890795L;
 
-		@Override
-		public void action() {
-			/*
-			 * only messages containing CALL-FOR-PROPOSAL in "performative" slot will be
-			 * processed
-			 */
-			MessageTemplate msgTemplate = MessageTemplate.MatchPerformative(ACLMessage.CFP);
-			ACLMessage msg = receive(msgTemplate);
-			if (msg != null) {
-				/* create reply for incoming message with price at "content" slot */
-				ACLMessage reply = msg.createReply();
-				reply.setPerformative(ACLMessage.PROPOSE);
-				int price = new Random().nextInt(100);
-				reply.setContent(String.valueOf(price));
+		public HandleCallForProposal(Agent a) {
+			super(a, createMessageTemplate(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET));
+		}
 
-				System.out.println(String.format("my price is %d", price));
-				/* send reply for incoming message */
-				send(reply);
-			} else {
-				/* wait till there is message matching template in message-queue */
-				block();
-			}
+		@Override
+		protected ACLMessage handleCfp(ACLMessage cfp) throws NotUnderstoodException, RefuseException {
+			ACLMessage reply = cfp.createReply();
+			reply.setPerformative(ACLMessage.PROPOSE);
+			reply.setContent("ok, master");
+			return reply;
+		}
+
+		@Override
+		protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose, ACLMessage accept)
+				throws FailureException {
+			ACLMessage inform = accept.createReply();
+			inform.setPerformative(ACLMessage.INFORM);
+
+			System.out.println("printing...");
+			return inform;
 		}
 	}
 
