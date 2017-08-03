7d9745719c97bd48d5b34c4064f665b186995a99
Mareike Redder <mareike.redder@fh-luebeck.de>
2017-08-03 11:24:47 +0200

    CM to Robots interaction is missing.

--- src/main/java/HomeWork/CoffeeMaschine.java
+++ src/main/java/HomeWork/CoffeeMaschine.java
@@ -22,9 +22,9 @@ import jade.lang.acl.MessageTemplate;
 import jade.proto.AchieveREInitiator;
 import jade.proto.AchieveREResponder;
 
-
 public class CoffeeMaschine extends Agent {
 	String Conversation1;
+
 	protected void setup() {
 		// description
 		ServiceDescription serviceDescription = new ServiceDescription();
@@ -53,38 +53,45 @@ public class CoffeeMaschine extends Agent {
 			// TODO Auto-generated constructor stub
 		}
 
+		// ######################################## Somewhere here probably Start
+		// #####################################
 		@Override
 		protected ACLMessage prepareResponse(ACLMessage request) throws NotUnderstoodException, RefuseException {
 			// send AGREE
 			Conversation1 = request.getContent();
 			ACLMessage agree = request.createReply();
 			agree.setPerformative(ACLMessage.AGREE);
-			System.out.println("CoffeeMaschine: My task is to do the "+ Conversation1.toString());
+			System.out.println("CoffeeMaschine: My task is to do the " + Conversation1.toString());
 			return agree;
 			// send REFUSE
 			// throw new RefuseException("check-failed");
 		}
+
 		@Override
-		protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) throws FailureException {
+		protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response)
+				throws FailureException {
 			ACLMessage inform = request.createReply();
-			System.out.println("CoffeeMaschine: "+ Conversation1+" is done! Give Waitress...");
-				inform.setContent(Conversation1);
-				inform.setPerformative(ACLMessage.INFORM);
+			System.out.println("CoffeeMaschine: " + Conversation1 + " is done! Give Waitress...");
+			inform.setContent(Conversation1);
+			inform.setPerformative(ACLMessage.INFORM);
 
 			return inform;
 		}
+
+		// ######################################## Somewhere here probably End
+		// #####################################
 		private static final long serialVersionUID = -8009542545033008746L;
 	}
-/*	
+
 	public ACLMessage starterMessage;
 
-	class Recipes{
+	class Recipes {
 		public Map<String, List<String>> menu = new HashMap<String, List<String>>();
 		List<String> CoffeeWithMilkList = new ArrayList<String>();
 		List<String> CoffeeList = new ArrayList<String>();
 		List<String> TeaList = new ArrayList<String>();
-		
-		public Recipes () {
+
+		public Recipes() {
 			CoffeeWithMilkList.add("boiling water");
 			CoffeeWithMilkList.add("grinding beans");
 			CoffeeWithMilkList.add("bringing milk");
@@ -96,7 +103,7 @@ public class CoffeeMaschine extends Agent {
 			TeaList.add("bringing tea leaves");
 			menu.put("Tea", TeaList);
 		}
-		
+
 		public Map<String, List<String>> getRecipe() {
 			return menu;
 		}
@@ -104,7 +111,7 @@ public class CoffeeMaschine extends Agent {
 
 	class ExecuteOrders extends TickerBehaviour {
 		private static final long serialVersionUID = -1534610326024914625L;
-		
+
 		public String obj;
 		public int recipeLen;
 		public int cnt = 0;
@@ -119,12 +126,12 @@ public class CoffeeMaschine extends Agent {
 			System.out.println("\nLooking for robots to make " + obj);
 			Recipes menu = new Recipes();
 			List<String> recipe = menu.getRecipe().get(obj);
-			
+
 			recipeLen = recipe.size();
 			cnt = 0;
-			
+
 			for (String ing : recipe) {
-				System.out.println("Looking for robots for " + ing);				
+				System.out.println("Looking for robots for " + ing);
 				List<AID> agents = findAgents(ing);
 				if (!agents.isEmpty()) {
 					System.out.println("Robots are found. Their names:");
@@ -132,7 +139,7 @@ public class CoffeeMaschine extends Agent {
 						System.out.println(ag.getLocalName());
 					}
 					System.out.println("Starting to make " + obj);
-					
+
 					String requestedAction = "work!";
 					ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
 					msg.setConversationId(ing);
@@ -147,7 +154,7 @@ public class CoffeeMaschine extends Agent {
 				}
 			}
 		}
-		
+
 		@Override
 		public void stop() {
 			System.out.println(obj + " is done");
@@ -175,7 +182,7 @@ public class CoffeeMaschine extends Agent {
 
 		class RequestToExecute extends AchieveREInitiator {
 			private static final long serialVersionUID = -8104498062148279796L;
-			
+
 			public RequestToExecute(Agent a, ACLMessage msg) {
 				super(a, msg);
 			}
@@ -195,5 +202,4 @@ public class CoffeeMaschine extends Agent {
 			}
 		}
 	}
-*/
 }
